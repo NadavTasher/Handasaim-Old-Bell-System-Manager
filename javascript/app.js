74258bec02e92619fd003fe39c21178541a8c1b4
nadav <tashernadav@gmail.com>
2019-02-16 18:02:21 +0200

    Things

--- javascript/app.js
+++ javascript/app.js
@@ -23,29 +23,34 @@ function login() {
 }
 
 function upload() {
-    let reader = new FileReader();
-    reader.readAsText(get("file").files[0], "UTF-8");
-    reader.onload = function (evt) {
-        let form = new FormData();
-        form.append("audio", btoa(evt.target.result));
-        form.append("name", get("name").value);
-        form.append("key", get("key").value);
-        form.append("second", get("second").value);
-        form.append("index", get("time").value);
-        fetch("php/modify.php", {
-            method: "post",
-            body: form
-        }).then(response => {
-            response.text().then((result) => {
-                let json = JSON.parse(result);
-                if (json.hasOwnProperty("success")) {
-                    if (json.success) {
-                        alert("Saved!");
-                    } else {
-                        alert("Not Saved");
-                    }
+    let form = new FormData();
+    form.append("name", get("name").value);
+    form.append("key", get("key").value);
+    form.append("second", get("second").value);
+    form.append("index", get("time").value);
+    form.append("audio", get("file").files[0]);
+    fetch("php/modify.php", {
+        method: "post",
+        body: form,
+        // headers: {
+        //     'Content-Type': 'application/x-www-form-urlencoded'
+        // }
+    }).then(response => {
+        response.text().then((result) => {
+            let json = JSON.parse(result);
+            if (json.hasOwnProperty("success")) {
+                if (json.success) {
+                    out("Saved!");
+                } else {
+                    out("Not Saved");
                 }
-            });
+            }
         });
-    };
+    });
+    out("Upload started.");
+}
+
+function out(text){
+    get("output").innerText=text;
+    view("output");
 }
--- php/modify.php
+++ php/modify.php
@@ -9,21 +9,20 @@ if (isset($_POST["key"])) {
     $result->auth = false;
     if (md5($_POST["key"]) === $settings->password) {
         $result->auth = true;
-        if (isset($_POST["name"]) && isset($_POST["second"]) && isset($_POST["index"]) && isset($_POST["audio"])) {
+        if (isset($_POST["name"]) && isset($_POST["second"]) && isset($_POST["index"])) {
             $result->success = false;
             $name = $_POST["name"];
             $second = $_POST["second"];
             $index = intval($_POST["index"], 10);
-            $audio = base64_decode($_POST["audio"]);
+            move_uploaded_file($_FILES['audio']['tmp_name'], audioDirectory . $name . ".mp3");
             for ($i = 0; $i < sizeof($database->queue); $i++) {
                 if ($i === $index) {
                     $database->queue[$i]->link = $name . ".mp3";
                     $database->queue[$i]->time = floatval($second);
-                    $database->queue[$i]->md5 = md5($audio);
+                    $database->queue[$i]->md5 = md5_file(audioDirectory . $name . ".mp3");
                     $result->success = true;
                 }
             }
-            file_put_contents(audioDirectory . $name . ".mp3", $audio);
             file_put_contents(databaseFile, json_encode($database));
         }
     }
