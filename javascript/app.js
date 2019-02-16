826d5cf94d9822596c43bd4863f82024d7044e53
nadav <tashernadav@gmail.com>
2019-02-16 16:11:35 +0200

    Things

--- javascript/app.js
+++ javascript/app.js
@@ -23,25 +23,29 @@ function login() {
 }
 
 function upload() {
-    let form = new FormData();
-    form.append("audio", btoa(get("file").files[0]));
-    form.append("name", get("name").value);
-    form.append("key", get("key").value);
-    form.append("second", get("second").value);
-    form.append("index", get("time").value);
-    fetch("php/modify.php", {
-        method: "post",
-        body: form
-    }).then(response => {
-        response.text().then((result) => {
-            let json = JSON.parse(result);
-            if (json.hasOwnProperty("success")) {
-                if (json.success) {
-                    alert("Saved!");
-                } else {
-                    alert("Not Saved");
+    let reader = new FileReader();
+    reader.readAsText(get("file").files[0], "UTF-8");
+    reader.onload = function (evt) {
+        let form = new FormData();
+        form.append("audio", btoa(evt.target.result));
+        form.append("name", get("name").value);
+        form.append("key", get("key").value);
+        form.append("second", get("second").value);
+        form.append("index", get("time").value);
+        fetch("php/modify.php", {
+            method: "post",
+            body: form
+        }).then(response => {
+            response.text().then((result) => {
+                let json = JSON.parse(result);
+                if (json.hasOwnProperty("success")) {
+                    if (json.success) {
+                        alert("Saved!");
+                    } else {
+                        alert("Not Saved");
+                    }
                 }
-            }
+            });
         });
-    });
+    };
 }
